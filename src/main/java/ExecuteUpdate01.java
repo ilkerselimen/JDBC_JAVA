a5f28cc44468114949f8cd76d6147715839c03f8
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-07 22:01:33 +0300

    v02

---
+++ src/main/java/ExecuteUpdate01.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class ExecuteUpdate01 {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db", "dev_user", "password");
+        Statement st = con.createStatement();
+
+        //ÖRNEK1:developers tablosunda maaşı ortalama maaştan az olanların maaşını ortalama maaş ile güncelleyiniz
+        String query="update developers set salary=(select avg(salary) from developers)" +
+                " where salary<(select avg(salary) from developers)";
+//        int updated=st.executeUpdate(query);
+//        System.out.println("Guncellenen kayit sayisi: "+updated);
+
+        ResultSet rs1=st.executeQuery("select id,name,salary from developers order by id");
+        while (rs1.next()){
+            System.out.println(rs1.getString("id")+"--"+rs1.getString("name")+"--"+rs1.getDouble("salary"));
+        }
+
+        //ÖRNEK2:developers tablosuna yeni bir developer ekleyiniz.
+        String query2="INSERT INTO developers(name,salary,prog_lang) VALUES('İlker',5300,'React')";
+//        int inserted = st.executeUpdate(query2);
+//        System.out.println("Eklenen kayit sayisi: "+inserted);
+
+        //ÖRNEK3:developers tablosundan id'si 14 olanı siliniz.
+        String query3="DELETE FROM developers where id=14";
+//        int deleted=st.executeUpdate(query3);
+//        System.out.println("Silinen kayit sayisi: "+deleted);
+
+    }
+}
